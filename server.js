f7fd9da56614429268c882ccedb6dd16867cac3f
antonybstack <ablyakhe@uncc.edu>
2020-05-02 21:33:54 -0400

    heroku deploy 8

--- server.js
+++ server.js
@@ -19,16 +19,16 @@ mongoose
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console("error!!!", err));
 
-app.listen(PORT, () => console.log("Server is running on Port: " + PORT));
-
 const tickets = require("./routes/api/tickets");
 app.use("/api/tickets", tickets);
 
 //serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("../frontend/build"));
+  app.use(express.static("frontend/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html")); ///////////////////////////
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
+
+app.listen(PORT, () => console.log("Server is running on Port: " + PORT));
